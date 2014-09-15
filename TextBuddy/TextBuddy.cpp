81a420d48f7a4a865e90b5723371f4a5bf0ab26a
cyeow <cheryl.ylt+git@gmail.com>
2014-09-15 18:44:55 +0800

    Improved code from CE1
    Changes to storage format: Using vectors then writing to file
    Improved SLAP
    Added comments to existing code
    Used magic strings and numbers
    Included checks for exception handling (at command line and input)
    
    new functions:
    void inputCommand(string);
    vector<string>::iterator getLineIter(string, string);
    void writeToFile(string);
    void printLine(int, string);
    bool checkInvalidInput(string;
    bool checkCLI(int);
    void initStore(string);

---
+++ TextBuddy/TextBuddy.cpp
@@ -0,0 +1,290 @@
+#include "stdafx.h"
+#include "TextBuddy.h"
+
+
+// TextBuddy.cpp:
+// TextBuddy is used to store, retrieve and delete lines in a textfile. 
+// The command format is given by the example interaction below:
+//
+// Welcome to TextBuddy. <filename>.txt is ready for use
+// command: add a quick brown fox
+// added to <filename>.txt: "a quick brown fox"
+// command: add jumped over a lazy dog
+// added to <filename>.txt: "jumped over a lazy dog"
+// command: display
+// 1. a quick brown fox
+// 2. jumped over a lazy dog
+// command: delete 1
+// deleted from <textfile>.txt: "a quick brown fox"
+// command: display
+// 1. jumped over a lazy dog
+// command: clear
+// all content deleted from <textfile>.txt
+// command: exit
+// 
+// author: Yeow Li Teng Cheryl 
+
+#include "stdafx.h"
+#include "TextBuddy.h"
+
+using namespace std;
+
+const string TextBuddy::MESSAGE_EMPTY = "%s is empty";
+const string TextBuddy::MESSAGE_ADDED = "added to %s: \"%s\"";
+const string TextBuddy::MESSAGE_DELETED = "deleted from %s: \"%s\"";
+const string TextBuddy::MESSAGE_CLEARED = "all content deleted from %s";
+const string TextBuddy::MESSAGE_LINE_NOT_FOUND = "line not found in %s";
+const string TextBuddy::MESSAGE_INVALID_FORMAT = "invalid command format: %s\n1. add <text>\n2. delete <line number>\n3. clear\n4. display\n5. sort\n6. search <word(s)>\n7. exit\n";
+const string TextBuddy::MESSAGE_PROGRAM_TERMINATION = "Enter any key to exit: ";
+const string TextBuddy::MESSAGE_WELCOME = "Welcome to TextBuddy. %s is ready for use";
+const string TextBuddy::ERROR_UNRECOGNISED_COMMAND_TYPE = "ERROR: Unrecognised command type.\n1. add <text>\n2. delete <line number>\n3. clear\n4. display\n5. sort\n6. search <word(s)>\n7. exit";
+const string TextBuddy::ERROR_USAGE = "ERROR: Usage: textbuddy.exe <filename>.txt";
+
+char TextBuddy::buffer[MAX_BUFFER_CHARACTERS] = "";
+vector<string> TextBuddy::store;
+
+void TextBuddy::main(int argc, char* argv[]) {
+	string filename;
+
+	checkCLI(argc);
+
+	initStore(argv[TEXTFILE_NAME]);
+
+	sprintf_s(buffer, MESSAGE_WELCOME.c_str(), argv[TEXTFILE_NAME]);
+	showToUser(buffer);
+
+	inputCommand(argv[TEXTFILE_NAME]);
+
+	return;
+}
+
+// receives user input and passes to executeCommand
+void TextBuddy::inputCommand(string filename){
+
+	while (true) {
+		cout << "command:";
+
+		string userCommand;
+		getline(cin, userCommand);
+
+		string feedback = executeCommand(filename, userCommand);
+		writeToFile(filename);
+		showToUser(feedback);
+	}
+
+	return;
+}
+
+// parses the command and passes it to the appropriate operation
+string TextBuddy::executeCommand(string filename, string userCommand) {
+	CommandType command;
+	string content;
+
+	command = determineCommandType(getFirstWord(userCommand));
+	content = removeFirstWord(userCommand);
+
+
+	switch (command) {
+	case ADD_LINE:
+		if (checkInvalidInput(content)) {
+			sprintf_s(buffer, MESSAGE_INVALID_FORMAT.c_str(), getFirstWord(userCommand).c_str());
+			return buffer;
+		}
+		return addLine(filename, content);
+
+	case DISPLAY_ALL:
+		return displayAll(filename);
+
+	case DELETE_LINE:
+		if (checkInvalidInput(content)) {
+			sprintf_s(buffer, MESSAGE_INVALID_FORMAT.c_str(), command);
+			return buffer;
+		}
+		return deleteLine(filename, content);
+
+	case CLEAR_ALL:
+		return clearAll(filename);
+
+	case EXIT:
+		exit(0);
+
+	case INVALID:
+		sprintf_s(buffer, MESSAGE_INVALID_FORMAT.c_str(), userCommand.c_str());
+		return buffer;
+
+	default:
+		showToUser(ERROR_UNRECOGNISED_COMMAND_TYPE);
+		showToUser(MESSAGE_PROGRAM_TERMINATION);
+
+		getchar();
+		exit(EXIT_FAILURE);
+	}
+
+}
+
+// adds a line to the text file
+string TextBuddy::addLine(string filename, string content) {
+
+	store.push_back(content);
+
+	sprintf_s(buffer, MESSAGE_ADDED.c_str(), filename.c_str(), content.c_str());
+
+	return buffer;
+}
+
+// displays all content in the text file
+string TextBuddy::displayAll(string filename) {
+
+	int lineCount = 1; 
+
+	if (store.begin() == store.end()) {
+		sprintf_s(buffer, MESSAGE_EMPTY.c_str(), filename.c_str());
+		return buffer;
+	}
+
+	for (vector<string>::iterator i = store.begin(); i != store.end(); i++) {
+		printLine(lineCount, *i);
+		lineCount++;
+	}
+
+	return "";
+}
+
+// deletes a line (stated) in the text file
+string TextBuddy::deleteLine(string filename, string content) {
+
+	vector<string>::iterator i = getLineIter(filename, content);
+
+	sprintf_s(buffer, MESSAGE_DELETED.c_str(), filename.c_str(), i->c_str());
+
+	store.erase(i);
+
+	return buffer;
+}
+
+// deletes all content in the text file
+string TextBuddy::clearAll(string filename) {
+
+	store.clear();
+
+	sprintf_s(buffer, MESSAGE_CLEARED.c_str(), filename.c_str());
+
+	return buffer;
+}
+
+
+// initialises the vector and *.txt 
+void TextBuddy::initStore(string filename) {
+	ifstream inFile;
+	string line;
+
+	inFile.open(filename);
+
+	while (getline(inFile, line)) {
+		store.push_back(line);
+	}
+
+	inFile.close();
+
+	return;
+}
+
+// returns iterator of line number requested (0 <= line number < n)
+vector<string>::iterator TextBuddy::getLineIter(string filename, string content) {
+	int iter = 1;
+
+	for (vector<string>::iterator i = store.begin(); i != store.end(); i++) {
+		if (iter == stoi(content)) {
+			return i;
+		}
+		else
+			iter++;
+	}
+
+	sprintf_s(buffer, MESSAGE_LINE_NOT_FOUND.c_str(), filename.c_str());
+
+	showToUser(buffer);
+
+	return store.end();
+}
+
+TextBuddy::CommandType TextBuddy::determineCommandType(string command) {
+	transform(command.begin(), command.end(), command.begin(), ::tolower);
+
+	if (command == "add"){
+		return CommandType::ADD_LINE;
+	}
+	else if (command == "display") {
+		return CommandType::DISPLAY_ALL;
+	}
+	else if (command == "delete") {
+		return CommandType::DELETE_LINE;
+	}
+	else if (command == "clear") {
+		return CommandType::CLEAR_ALL;
+	}
+	else if (command == "exit") {
+		return CommandType::EXIT;
+	}
+	else
+		return CommandType::INVALID;
+}
+
+bool TextBuddy::checkInvalidInput(string content) {
+	if (content == "" || content == " ") {
+		return true;
+	}
+	return false;
+}
+
+void TextBuddy::checkCLI(int argc) {
+	if (argc != MAX_ARGUMENTS) {
+		// if the command line input is wrong, generates an error message and terminates the program.
+		showToUser(ERROR_USAGE);
+		showToUser(MESSAGE_PROGRAM_TERMINATION);
+
+		getchar();
+		exit(0);
+	}
+}
+
+string TextBuddy::removeFirstWord(string command){
+	if (command.find_first_of(" ") == string::npos) {
+		return "";
+	}
+	return command.substr(command.find_first_of(" ") + 1);
+}
+
+string TextBuddy::getFirstWord(string command){
+	return command.substr(0, command.find(' '));
+}
+
+void TextBuddy::printLine(int i, string line) {
+	cout << i << ". " << line << endl;
+}
+
+void TextBuddy::showToUser(string text) {
+	cout << text << endl;
+}
+
+// transfers strings in vector to file
+void TextBuddy::writeToFile(string filename) {
+	ofstream file;
+
+	file.open(filename);
+	file.close();
+
+	if (store.begin() != store.end()) {
+		for (vector<string>::iterator i = store.begin(); i != store.end(); i++) {
+			file.open(filename, ios::app);
+			file << *i << endl;
+			file.close();
+		}
+	}
+
+	return;
+}
+
+int main(int argc, char* argv[]) {
+	TextBuddy::main(argc, argv);
+}
---
+++ TextBuddy/TextBuddy.h
@@ -0,0 +1,91 @@
+#pragma once
+// CE1TextBuddy.cpp:
+// TextBuddy is used to store, retrieve and delete lines in a textfile. 
+// The command format is given by the example interaction below:
+//
+// Welcome to TextBuddy. <filename>.txt is ready for use
+// command: add a quick brown fox
+// added to <filename>.txt: "a quick brown fox"
+// command: add jumped over a lazy dog
+// added to <filename>.txt: "jumped over a lazy dog"
+// command: display
+// 1. a quick brown fox
+// 2. jumped over a lazy dog
+// command: delete 1
+// deleted from <textfile>.txt: "a quick brown fox"
+// command: display
+// 1. jumped over a lazy dog
+// command: clear
+// all content deleted from <textfile>.txt
+// command: exit
+// 
+// author: Yeow Li Teng Cheryl 
+
+#ifndef TEXTBUDDY_H
+#define TEXTBUDDY_H
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+const int MAX_ARGUMENTS = 2;
+const int MAX_BUFFER_CHARACTERS = 255;
+const int TEXTFILE_NAME = 1;
+const int FILE_IS_EMPTY = 1;
+
+class TextBuddy {
+
+public:
+	enum CommandType {
+		ADD_LINE, DISPLAY_ALL, DELETE_LINE, CLEAR_ALL, EXIT, INVALID
+	};
+
+	static void main(int argc, char* argv[]);
+
+	static void inputCommand(string filename);
+	static string executeCommand(string filename, string userCommand);
+
+	static string addLine(string filename, string content);
+	static string displayAll(string filename);
+	static string deleteLine(string filename, string content);
+	static string clearAll(string filename);
+
+	static vector<string>::iterator getLineIter(string filename, string content);
+
+	static CommandType determineCommandType(string command);
+
+	static string removeFirstWord(string command);
+	static string getFirstWord(string command);
+
+	static void writeToFile(string filename);
+	static void printLine(int i, string line);
+	static void showToUser(string text);
+
+	static bool checkInvalidInput(string content);
+	static void checkCLI(int argc);
+	static void initStore(string filename);
+
+
+private:
+	static const string MESSAGE_EMPTY;
+	static const string MESSAGE_ADDED;
+	static const string MESSAGE_DELETED;
+	static const string MESSAGE_CLEARED;
+	static const string MESSAGE_LINE_NOT_FOUND;
+	static const string MESSAGE_INVALID_FORMAT;
+	static const string MESSAGE_PROGRAM_TERMINATION;
+	static const string MESSAGE_WELCOME;
+	static const string ERROR_UNRECOGNISED_COMMAND_TYPE;
+	static const string ERROR_USAGE;
+
+	static char buffer[MAX_BUFFER_CHARACTERS];
+	static vector<string> store;
+
+};
+
+#endif
